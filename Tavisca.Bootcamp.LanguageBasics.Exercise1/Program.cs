5c83e2a05a3ecf59989f3d35cd906dd1516a4480
SHRILATA MONDAL <51924756+tavisca-smandal@users.noreply.github.com>
2019-06-22 17:01:31 +0530

    Update Program.cs
    Solution for Assignment 1- Shrilata Mondal

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,49 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            string a = equation.Substring(0, equation.IndexOf('*'));
+            string b = equation.Substring(equation.IndexOf('*') + 1, equation.IndexOf('=') - equation.IndexOf('*') - 1);
+            string c = equation.Substring(equation.IndexOf('=') + 1);
+            if (a.Contains('?'))
+            {
+                int value = (int.Parse(c)) / int.Parse(b);
+                if (value * int.Parse(b) == int.Parse(c))
+                {
+                    string result = value.ToString();
+                    if (result.Length == a.Length)
+                    {
+                        int index = a.IndexOf('?');
+                        return int.Parse(result[index].ToString());
+                    }
+                }
+            }
+            else if (b.Contains('?'))
+            {
+                int value = (int.Parse(c)) / int.Parse(a);
+                if (value * int.Parse(a) == int.Parse(c))
+                {
+                    string result = value.ToString();
+                    if (result.Length == b.Length)
+                    {
+                        int index = b.IndexOf('?');
+                        return int.Parse(result[index].ToString());
+                    }
+                }
+            }
+            else
+            {
+                int value = int.Parse(a) * int.Parse(b);
+                if (int.Parse(a) * int.Parse(b) == value)
+                {
+                    string result = value.ToString();
+                    if (result.Length == c.Length)
+                    {
+                        int index = c.IndexOf('?');
+                        return int.Parse(result[index].ToString());
+                    }
+                }
+            }
+            return -1;
         }
     }
 }
